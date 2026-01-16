9582f2b88a9d7d2b4fde660b091db8f01e093c98
MaxNickell <maxanickell@gmail.com>
2026-01-15 20:30:33 -0500

    max time duration study

--- single_image_website/app.py
+++ single_image_website/app.py
@@ -495,4 +495,4 @@ if __name__ == '__main__':
         load_images()
         cleanup_duplicates()
     
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=8080)
